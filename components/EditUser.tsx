4082bd09a03d6c41e61568dcc4cb8b39c9d18338
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2025-04-27 22:02:31 +0300

    react-hook-form with zod is ok

--- components/EditUser.tsx
+++ components/EditUser.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
 	SheetContent,
@@ -5,14 +6,146 @@ import {
 	SheetHeader,
 	SheetTitle
 } from "@/components/ui/sheet";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from "./ui/select";
+
+const formSchema = z.object({
+	username: z
+		.string()
+		.min(2, {
+			message: "Username must be at least 2 characters long"
+		})
+		.max(50),
+	email: z.string().email({ message: "Invalid email address" }),
+	phone: z.string().min(10).max(15),
+	location: z.string().min(2),
+	role: z.enum(["admin", "user"])
+});
 const EditUser = () => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: "Gökhan Suçsuz",
+			email: "gokhan.sucsuz@gmail.com",
+			phone: "05324567890",
+			location: "USA",
+			role: "admin"
+		}
+	});
+
 	return (
 		<SheetContent>
 			<SheetHeader>
 				<SheetTitle className="mb-4">Edit User</SheetTitle>
-				<SheetDescription>
-					This action cannot be undone. This will permanently delete your
-					account and remove your data from our servers.
+				<SheetDescription asChild>
+					<Form {...form}>
+						<form className="space-y-8">
+							<FormField
+								control={form.control}
+								name="username"
+								render={({ field }) =>
+									<FormItem>
+										<FormLabel>Username</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>
+											This is your public username.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>}
+							/>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) =>
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>
+											Only admin can see your email.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>}
+							/>
+							<FormField
+								control={form.control}
+								name="phone"
+								render={({ field }) =>
+									<FormItem>
+										<FormLabel>Phone</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>
+											Only admin can see your phone number.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>}
+							/>
+							<FormField
+								control={form.control}
+								name="location"
+								render={({ field }) =>
+									<FormItem>
+										<FormLabel>Location</FormLabel>
+										<FormControl>
+											<Input {...field} />
+										</FormControl>
+										<FormDescription>
+											This is the public location.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>}
+							/>
+							<FormField
+								control={form.control}
+								name="role"
+								render={({ field }) =>
+									<FormItem>
+										<FormLabel>Role</FormLabel>
+										<FormControl>
+											<Select>
+												<SelectTrigger>
+													<SelectValue placeholder="Role" />
+												</SelectTrigger>
+												<SelectContent>
+													<SelectItem value="admin">Admin</SelectItem>
+													<SelectItem value="user">User</SelectItem>
+												</SelectContent>
+											</Select>
+										</FormControl>
+										<FormDescription>
+											Only verified users can be admin.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>}
+							/>
+							<Button type="submit">Submit</Button>
+						</form>
+					</Form>
 				</SheetDescription>
 			</SheetHeader>
 		</SheetContent>
