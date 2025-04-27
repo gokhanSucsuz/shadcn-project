824de2c96748aacfc0c0aa301f9a0774cf108a3c
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2025-04-27 18:52:18 +0300

    shadcn-components

---
+++ components/AppSidebar.tsx
@@ -0,0 +1,245 @@
+"use client";
+import React from "react";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupAction,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuBadge,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+	SidebarSeparator,
+	useSidebar
+} from "@/components/ui/sidebar";
+
+import {
+	Home,
+	Inbox,
+	Search,
+	Calendar,
+	Settings,
+	User2,
+	ChevronUp,
+	Plus,
+	ChevronsUpDown,
+	Projector,
+	ChevronsUp
+} from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+	DropdownMenuContent,
+	DropdownMenuTrigger
+} from "@radix-ui/react-dropdown-menu";
+import {
+	DropdownMenu,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator
+} from "./ui/dropdown-menu";
+
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger
+} from "@/components/ui/collapsible";
+import { Button } from "./ui/button";
+
+const items = [
+	{
+		title: "Home",
+		url: "/",
+		icon: Home
+	},
+	{
+		title: "Inbox",
+		url: "#",
+		icon: Inbox
+	},
+	{
+		title: "Calendar",
+		url: "#",
+		icon: Calendar
+	},
+	{
+		title: "Search",
+		url: "#",
+		icon: Search
+	},
+	{
+		title: "Settings",
+		url: "#",
+		icon: Settings
+	}
+];
+const AppSidebar = () => {
+	return (
+		<Sidebar collapsible="icon">
+			<SidebarHeader className="py-4">
+				<SidebarMenu>
+					<SidebarMenuButton
+						asChild
+						className="flex items-center justify-start"
+					>
+						<Link href="/" className="flex items-center gap-3">
+							<Image
+								src="/logo.png"
+								alt="Logo"
+								width={32}
+								height={32}
+								className="rounded-full"
+							/>
+							Shadcn Project
+						</Link>
+					</SidebarMenuButton>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarSeparator />
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>ITEMS</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item, index) =>
+								<SidebarMenuItem
+									key={index}
+									className="flex items-center gap-3"
+								>
+									<SidebarMenuButton asChild>
+										<Link href={item.url} className="flex items-center gap-3">
+											<item.icon className="h-4 w-4" />
+											<span>
+												{item.title}
+											</span>
+										</Link>
+									</SidebarMenuButton>
+									{item.title === "Inbox" &&
+										<SidebarMenuBadge>24</SidebarMenuBadge>}
+								</SidebarMenuItem>
+							)}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+
+				<SidebarGroup>
+					<SidebarGroupLabel>Projects</SidebarGroupLabel>
+					<SidebarGroupAction>
+						<Plus /> <span className="sr-only">Add Project</span>
+					</SidebarGroupAction>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link href="#">
+										<Plus />
+										Add Projects
+									</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				<Collapsible defaultOpen className="group/collapsible">
+					<SidebarGroup>
+						<SidebarGroupLabel>
+							<CollapsibleTrigger className="w-full flex items-center">
+								<span>Collapsible Content</span>
+								<ChevronUp className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+							</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<SidebarGroupContent>
+							<CollapsibleContent className="space-y-2">
+								<SidebarMenu>
+									<SidebarMenuItem>
+										<SidebarMenuButton asChild>
+											<Link href="#">
+												<Projector />
+												See All Projects
+											</Link>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								</SidebarMenu>
+								<SidebarMenu>
+									<SidebarMenuItem>
+										<SidebarMenuButton asChild>
+											<Link href="#">
+												<Plus />
+												Add Projects
+											</Link>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								</SidebarMenu>
+							</CollapsibleContent>
+							<SidebarGroup>
+								<SidebarGroupLabel>Projects</SidebarGroupLabel>
+								<SidebarGroupContent>
+									<SidebarMenu>
+										<SidebarMenuItem>
+											<SidebarMenuButton asChild>
+												<Link href="#">
+													<Projector />
+													See All Projects
+												</Link>
+											</SidebarMenuButton>
+											<SidebarMenuSub>
+												<SidebarMenuSubItem>
+													<SidebarMenuSubButton asChild>
+														<Link href="#">
+															<Plus />
+															Add Projects
+														</Link>
+													</SidebarMenuSubButton>
+												</SidebarMenuSubItem>
+												<SidebarMenuSubItem>
+													<SidebarMenuSubButton asChild>
+														<Link href="#">
+															<Plus />
+															Add Category
+														</Link>
+													</SidebarMenuSubButton>
+												</SidebarMenuSubItem>
+											</SidebarMenuSub>
+										</SidebarMenuItem>
+									</SidebarMenu>
+								</SidebarGroupContent>
+							</SidebarGroup>
+						</SidebarGroupContent>
+					</SidebarGroup>
+				</Collapsible>
+			</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton>
+									<User2 className="h-4 w-4" />
+									Gökhan Suçsuz
+									<ChevronUp />
+								</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuLabel>My Account</DropdownMenuLabel>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>Profile</DropdownMenuItem>
+								<DropdownMenuItem>Billing</DropdownMenuItem>
+								<DropdownMenuItem>Team</DropdownMenuItem>
+								<DropdownMenuItem>Subscription</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
+};
+
+export default AppSidebar;
---
+++ components/Navbar.tsx
@@ -0,0 +1,91 @@
+"use client";
+import logo from "@/public/logo.png";
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+	LogOut,
+	Moon,
+	Sun,
+	Settings,
+	User,
+	ToggleLeftIcon
+} from "lucide-react";
+import Link from "next/link";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { SidebarTrigger } from "./ui/sidebar";
+const Navbar = () => {
+	const { setTheme } = useTheme();
+	// const { toggleSidebar } = useSidebar();
+	return (
+		<nav className="flex p-4 items-center justify-between">
+			<SidebarTrigger className="cursor-pointer" />
+			{/* <Button variant="outline" onClick={toggleSidebar}>
+				<ToggleLeftIcon className="h-4 w-4" />
+				Toggle Button
+			</Button> */}
+			<div className="flex items-center gap-4">
+				<Link href="/">Dashboard</Link>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="outline" size="icon">
+							<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+							<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+							<span className="sr-only">Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem onClick={() => setTheme("light")}>
+							Light
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme("dark")}>
+							Dark
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme("system")}>
+							System
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+				<DropdownMenu>
+					<DropdownMenuTrigger className="cursor-pointer rounded-full">
+						<Avatar>
+							<AvatarImage
+								className="h-8 w-8 rounded-full"
+								src={
+									"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFW0NbncSWEnZ83TNSs2LnzrIvoLcDCBU2pw&s"
+								}
+							/>
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent sideOffset={10}>
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<User className="h-[1.2rem] w-[1.2rem] mr-2" />
+							Profile
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+							Settings
+						</DropdownMenuItem>
+						<DropdownMenuItem variant="destructive">
+							<LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+							Logout
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
