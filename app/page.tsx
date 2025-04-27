29277f2cf68ed22365896f85ba8663285aa2c8e0
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2025-04-27 19:26:20 +0300

    card component is ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { AppAreaChart } from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
 import { AppPieChart } from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 export default function Home() {
 	return (
@@ -8,15 +9,19 @@ export default function Home() {
 			<div className="bg-primary-foreground p-4 rounded-lg log:col-span-2 xl:col-span-1 2xl:col-span-2">
 				<AppBarChart />
 			</div>
-			<div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+			<div className="bg-primary-foreground p-4 rounded-lg">
+				<CardList title="Latest Transactions" />
+			</div>
 			<div className="bg-primary-foreground p-4 rounded-lg">
 				<AppPieChart />
 			</div>
-			<div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+			<div className="bg-primary-foreground p-4 rounded-lg" />
 			<div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
 				<AppAreaChart />
 			</div>
-			<div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+			<div className="bg-primary-foreground p-4 rounded-lg">
+				<CardList title="Latest Transactions" />
+			</div>
 		</div>
 	);
 }
