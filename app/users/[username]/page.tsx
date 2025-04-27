9d2fb838d1d67999f2d404aa8ba0f2822ac8d3f0
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2025-04-27 21:21:36 +0300

    progressbar is ok

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -14,6 +14,8 @@ import {
 } from "@/components/ui/hover-card";
 import CardList from "@/components/CardList";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { Badge } from "@/components/ui/badge";
 const UsersDetailPage = () => {
 	return (
 		<div>
@@ -99,7 +101,40 @@ const UsersDetailPage = () => {
 						</div>
 					</div>
 					{/* INFORMATION CONTAINER */}
-					<div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+					<div className="bg-primary-foreground p-4 rounded-lg">
+						<h1 className="text-xl font-semibold">User Information</h1>
+						<div className="space-y-4 mt-4">
+							<div className="flex flex-col gap-2 mb-4">
+								<p className="text-sm text-muted-foreground">
+									Profile Completion
+								</p>
+								<Progress value={75} />
+								<div className="flex items-center gap-2">
+									<span className="font-bold">Username: </span>
+									<span>Gökhan.Suçsuz </span>
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="font-bold">Email: </span>
+									<span>gokhansucsuz@gmail.com </span>
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="font-bold">Phone: </span>
+									<span>+90 542 585 25 78 </span>
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="font-bold">Location: </span>
+									<span>New Jersey, USA </span>
+								</div>
+								<div className="flex items-center gap-2">
+									<span className="font-bold">Role: </span>
+									<Badge>Admin</Badge>
+								</div>
+								<p className="text-sm text-muted-foreground mt-4">
+									Joined on 2025.01.01
+								</p>
+							</div>
+						</div>
+					</div>
 					{/* CARD LIST CONTAINER */}
 					<div className="bg-primary-foreground p-4 rounded-lg">
 						<CardList title="Latest Transactions" />
