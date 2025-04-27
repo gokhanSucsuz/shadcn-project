12b7334c40ae9834c344713fea0813d3f5a8ba9b
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2025-04-27 22:54:24 +0300

    project is finished...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,9 @@
 "use client";
-import logo from "@/public/logo.png";
 import * as React from "react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import {
-	LogOut,
-	Moon,
-	Sun,
-	Settings,
-	User,
-	ToggleLeftIcon
-} from "lucide-react";
+import { LogOut, Moon, Sun, Settings, User, Menu } from "lucide-react";
 import Link from "next/link";
 import {
 	DropdownMenu,
@@ -21,18 +13,59 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger
+} from "@/components/ui/sheet";
 import { SidebarTrigger } from "./ui/sidebar";
 const Navbar = () => {
 	const { setTheme } = useTheme();
+	const [open, setOpen] = React.useState(false);
 	// const { toggleSidebar } = useSidebar();
-	return <nav className="flex p-4 items-center justify-between sticky top-0 bg-background z-10">
+	return (
+		<nav className="flex p-4 items-center justify-between sticky top-0 bg-background z-10">
 			<SidebarTrigger className="cursor-pointer" />
 			{/* <Button variant="outline" onClick={toggleSidebar}>
 				<ToggleLeftIcon className="h-4 w-4" />
 				Toggle Button
 			</Button> */}
-			<div className="flex items-center gap-4">
+			<div className="hidden md:flex items-center gap-4">
 				<Link href="/">Dashboard</Link>
+				<Link href="/users">Users</Link>
+				<Link href="/users/admin">Admin</Link>
+				<Link href="/payments">Payments</Link>
+			</div>
+			<Sheet open={open} onOpenChange={setOpen}>
+				<SheetTrigger className="flex md:hidden">
+					<Menu className="cursor-pointer" />
+				</SheetTrigger>
+				<SheetContent>
+					<SheetHeader>
+						<SheetTitle>Shadcn Project</SheetTitle>
+						<SheetDescription asChild>
+							<div className="flex flex-col items-center gap-4 py-8">
+								<Link onClick={() => setOpen(false)} href="/">
+									Dashboard
+								</Link>
+								<Link onClick={() => setOpen(false)} href="/users">
+									Users
+								</Link>
+								<Link onClick={() => setOpen(false)} href="/users/admin">
+									Admin
+								</Link>
+								<Link onClick={() => setOpen(false)} href="/payments">
+									Payments
+								</Link>
+							</div>
+						</SheetDescription>
+					</SheetHeader>
+				</SheetContent>
+			</Sheet>
+			<div className="flex items-center gap-4">
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="outline" size="icon">
@@ -56,7 +89,12 @@ const Navbar = () => {
 				<DropdownMenu>
 					<DropdownMenuTrigger className="cursor-pointer rounded-full">
 						<Avatar>
-							<AvatarImage className="h-8 w-8 rounded-full" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFW0NbncSWEnZ83TNSs2LnzrIvoLcDCBU2pw&s"} />
+							<AvatarImage
+								className="h-8 w-8 rounded-full"
+								src={
+									"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFW0NbncSWEnZ83TNSs2LnzrIvoLcDCBU2pw&s"
+								}
+							/>
 							<AvatarFallback>CN</AvatarFallback>
 						</Avatar>
 					</DropdownMenuTrigger>
@@ -78,7 +116,8 @@ const Navbar = () => {
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</div>
-		</nav>;
+		</nav>
+	);
 };
 
 export default Navbar;
